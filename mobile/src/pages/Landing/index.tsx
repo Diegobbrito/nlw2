f45aafaf1e353c9f9cbc3c8b180159b0bbc04536
Diego <dibbrito@gmail.com>
2020-08-08 15:13:08 -0300

    Started mobile app

---
+++ mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import styles from './styles';
+import landingImg from '../../assets/images/landing.png';
+
+function Landing(){
+    return <View style={styles.container}>
+        <Image source={landingImg}/>
+
+        <Text style={styles.title}>
+            Seja bem-vindo, {'\n'}
+            <Text style={styles.titleBold}>O que deseja fazer?</Text>
+        </Text>
+    </View>
+}
+
+export default Landing;
