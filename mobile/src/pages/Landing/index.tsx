418cecc4c7bbbea41daaac47b9df6dd5491439b5
Diego <dibbrito@gmail.com>
2020-08-09 00:50:56 -0300

    Add routes, pages

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
+import {useNavigation} from '@react-navigation/native'
+import {RectButton} from 'react-native-gesture-handler';
+
 import styles from './styles';
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
@@ -7,6 +10,14 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing(){
+    const { navigate } = useNavigation(); 
+
+    function handleNavigateToGiveClassesPage(){
+        navigate('GiveClasses');
+    }
+    function handleNavigateToStudyPages(){
+        navigate('Study');
+    }
     return <View style={styles.container}>
         <Image source={landingImg}/>
 
@@ -15,16 +26,16 @@ function Landing(){
             <Text style={styles.titleBold}>O que deseja fazer?</Text>
         </Text>
         <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+            <RectButton onPress={handleNavigateToStudyPages} style={[styles.button, styles.buttonPrimary]}>
                 <Image source={studyIcon}/>
                 <Text style={styles.buttonText}>Estudar</Text>
 
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+            </RectButton>
+            <RectButton onPress={handleNavigateToGiveClassesPage} style={[styles.button, styles.buttonSecondary]}>
                 <Image source={giveClassesIcon}/>
                 <Text style={styles.buttonText}>Dar aulas</Text>
 
-            </TouchableOpacity>
+            </RectButton>
         </View>
         <Text style={styles.totalConnections}>
             Total de x conexões já realizadas  {' '}
